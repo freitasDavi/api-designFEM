6b0eecfd14c4afe2efec0fc9c6e464330fc96b7e
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-09-25 17:03:24 -0300

    index + server

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/data', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
